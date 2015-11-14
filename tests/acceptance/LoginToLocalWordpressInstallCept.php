d84b60ad4996c1ad6bc98b2fdfb9b2cfcecf73ee
Hitman007 <support@customrayguns.com>
2015-11-14 17:21:05 -0500

    Update LoginToLocalWordpressInstallCept.php

--- tests/acceptance/LoginToLocalWordpressInstallCept.php
+++ tests/acceptance/LoginToLocalWordpressInstallCept.php
@@ -2,7 +2,5 @@
 
 include 'WordpressAcceptanceTester.class.php';
 $I = new CustomRayGuns\WordpressAcceptanceTester($scenario);
-$I->wantTo('Login to Wordpress on \localhost as an admin');
+$I->wantTo('Login to Wordpress as an admin');
 $I->loginWordpressAs('admin');
-//$I->see('Log In');
-//$I->amOnPage( wp_login_url() );
