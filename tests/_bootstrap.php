1f38ff9a051696b8687c3c389b6356545aae7773
Hitman007 <support@customrayguns.com>
2015-11-15 02:49:34 -0500

    Update _bootstrap.php

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,3 +1,5 @@
 <?php
 // This is global bootstrap for autoloading
 include('php-autoloader/autoloader.php');
+
+global $CRG_homePageURL = 'http://ec2-54-175-20-216.compute-1.amazonaws.com/wp-login.php';
