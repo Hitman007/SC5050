975317e2e9e63046a576f3d8c13b9cf19080a822
Hitman007 <support@customrayguns.com>
2015-11-14 21:16:33 -0500

    Update WooCommerceIsInstalledCept.php

--- tests/acceptance/WooCommerceIsInstalledCept.php
+++ tests/acceptance/WooCommerceIsInstalledCept.php
@@ -1,6 +1,6 @@
 <?php
 
 $I = new AcceptanceTester($scenario);
-$output = shell_exec("sudo wp plugin status Testable");
+$output = shell_exec("sudo wp plugin status Testable --allow-root");
 //$output = shell_exec("ls");
 throw new Exception($output);
