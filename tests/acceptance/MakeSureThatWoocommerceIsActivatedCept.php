d2f87555a0603580a6cee90126ef0558488672de
Hitman007 <support@customrayguns.com>
2015-11-14 22:46:19 -0500

    Update and rename WooCommerceIsActivatedCept.php to MakeSureThatWoocommerceIsActivatedCept.php

--- tests/acceptance/MakeSureThatWoocommerceIsActivatedCept.php
+++ tests/acceptance/MakeSureThatWoocommerceIsActivatedCept.php
@@ -3,5 +3,5 @@
 //This test fails if a given plugin is not active. WooCommerce
 
 $I = new CustomRayGuns\WordpressAcceptanceTester($scenario);
-$I->wantTo('Make sure that a Woocommerce is activated');
+$I->wantTo('Make sure that Woocommerce is activated');
 $I->seeThisPluginIsActivated('woocommerce');
