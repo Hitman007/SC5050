a62e5eb4636b817069831d610cae47c9bd3d5faa
Hitman007 <support@customrayguns.com>
2015-11-15 02:53:32 -0500

    Update _bootstrap.php

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -4,3 +4,9 @@ include('php-autoloader/autoloader.php');
 
 global $CRG_homePageURL;
 $CRG_homePageURL = 'http://ec2-54-175-20-216.compute-1.amazonaws.com/wp-login.php';
+
+global $CRG_adminRoleUserName;
+$CRG_adminRoleUserName = "";
+
+global $CRG_adminRoleUserPassword;
+$CRG_adminRoleUserPassword = "";
