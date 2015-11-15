b53965de05ec6492e0fcc05f8e0f75e49d16ac18
Hitman007 <support@customrayguns.com>
2015-11-15 02:06:30 -0500

    Create abilityToSeeActivatedPlugins.trait.php

---
+++ tests/acceptance/abilityToSeeActivatedPlugins.trait.php
@@ -0,0 +1,13 @@
+trait abilityToSeeActivatedPlugins{
+
+	function seeThisPluginIsActivated($pluginName){
+		$I = $this;
+		$x = "sudo wp plugin status $pluginName --allow-root";
+		$str = shell_exec($x);
+		if (strpos($str, 'Active') !== FALSE){
+		 }else{
+		 	$x = "The plugin $pluginName is not activated";
+    			throw new \Exception($x);
+		 }
+	}
+}
