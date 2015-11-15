7d9a845756faa980083a2c6e4a8a2847f96125b0
Hitman007 <support@customrayguns.com>
2015-11-14 21:27:18 -0500

    Update WooCommerceIsInstalledCept.php

--- tests/acceptance/WooCommerceIsInstalledCept.php
+++ tests/acceptance/WooCommerceIsInstalledCept.php
@@ -3,4 +3,8 @@
 $I = new AcceptanceTester($scenario);
 $I->wantTo('Make sure that a plugin is installed');
 $output = shell_exec("sudo wp plugin status Testable --allow-root");
-throw new Exception($output);
+
+if (strpos($str, 'Active') !== FALSE){}
+   else{
+    throw new Exception('The plugin is not active');
+   }
