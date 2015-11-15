dd3200821a89d42f52b5179a9788c95ccb7483c1
Hitman007 <support@customrayguns.com>
2015-11-14 22:25:11 -0500

    Update WooCommerceIsInstalledCept.php

--- tests/acceptance/WooCommerceIsInstalledCept.php
+++ tests/acceptance/WooCommerceIsInstalledCept.php
@@ -3,6 +3,7 @@
 //This test fails if a given plugin is not active.
 
 include_once 'WordpressAcceptanceTester.class.php';
+$scenario = "assmunching";
 $I = new CustomRayGuns\WordpressAcceptanceTester($scenario);
 $I->wantTo('Make sure that a plugin is installed');
 $I->seeActivatedPlugin('Testable');
