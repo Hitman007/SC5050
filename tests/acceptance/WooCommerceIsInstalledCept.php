da3b8fa846fc0a2cfa26a226950803becc0822c0
Hitman007 <support@customrayguns.com>
2015-11-14 20:11:01 -0500

    Update WooCommerceIsInstalledCept.php

--- tests/acceptance/WooCommerceIsInstalledCept.php
+++ tests/acceptance/WooCommerceIsInstalledCept.php
@@ -1,6 +1,7 @@
 <?php
 
 $I = new AcceptanceTester($scenario);
-$output = shell_exec ("wp plugin status Testable");
-$output = "oops";
+//$output = shell_exec("wp plugin status Testable");
+$output = shell_exec("ls");
+//$output = "oops";
 throw new Exception($output);
