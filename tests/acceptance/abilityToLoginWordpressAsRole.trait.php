a6a3b2c7cfe3f927563b33fd3a3b9c0ac5e0aa85
Hitman007 <support@customrayguns.com>
2015-11-15 02:15:50 -0500

    Create abilityToLoginWordpressAsRole.trait.php

---
+++ tests/acceptance/abilityToLoginWordpressAsRole.trait.php
@@ -0,0 +1,22 @@
+<?php
+
+trait abilityToLoginWordpressAsRole{
+
+	public function loginWordpressAs($role) {
+		$I = $this;
+		$I->amOnPage('http://ec2-54-175-20-216.compute-1.amazonaws.com/wp-login.php');
+		$I->see('Log In');
+		switch ($role) {
+			case "admin":
+				$I->fillField('log', 'Tester');
+				$I->fillField('pwd', 'Tester123$');
+				$I->click('Log In');
+				$I->see('Howdy');
+				break;
+			default:
+				throw new \Exception('login role not recognized');
+				break;
+		}
+	}
+	
+}
