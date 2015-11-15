887f3e8753e975324c2790a6c5a0b55eec80cef5
Hitman007 <support@customrayguns.com>
2015-11-15 03:02:03 -0500

    Update WordpressAcceptanceTester.class.php

--- tests/acceptance/WordpressAcceptanceTester.class.php
+++ tests/acceptance/WordpressAcceptanceTester.class.php
@@ -8,23 +8,4 @@ class WordpressAcceptanceTester extends \AcceptanceTester{
 	
 	use abilityToLoginWordpressAsRole;
 	
-	public function loginWordpressAs($role) {
-		$I = $this;
-		global $CRG_homePageURL; //This variable is set in the tests/_bootstrap.php file
-		$I->amOnPage($CRG_homePageURL);
-		$I->see('Log In');
-		switch ($role) {
-			case "admin":
-				global $CRG_adminRoleUserName; //This variable is set in the tests/_bootstrap.php file
-				$I->fillField('log', $CRG_adminRoleUserName);
-				global $CRG_adminRoleUserPassword; //This variable is set in the tests/_bootstrap.php file
-				$I->fillField('pwd', $CRG_adminRoleUserPassword);
-				$I->click('Log In');
-				$I->see('Howdy');
-				break;
-			default:
-				throw new \Exception('login role not recognized');
-				break;
-		}
-	}
 }
