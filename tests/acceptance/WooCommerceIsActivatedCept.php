89fa3a57e31837d43c2afb4073f0965f5e5af04d
Hitman007 <support@customrayguns.com>
2015-11-18 07:16:39 -0500

    fixed

--- tests/acceptance/WooCommerceIsActivatedCept.php
+++ tests/acceptance/WooCommerceIsActivatedCept.php
@@ -2,4 +2,4 @@
 //This test fails if a given plugin is not active. WooCommerce
 $I = new CustomRayGuns\WordpressAcceptanceTester($scenario);
 $I->wantTo('Make sure that Woocommerce is activated');
-$I->seeThisPluginIsActivated_('woocommerce');
+$I->thisPluginIsActivated('woocommerce');
