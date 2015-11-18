cb1a51b4de1b439f3109d703c11ec036e33eb0a7
Hitman007 <support@customrayguns.com>
2015-11-18 11:10:26 -0500

    Update SC5050.php

--- SC5050.php
+++ SC5050.php
@@ -7,8 +7,9 @@ Version: 0.1
 Author: Jim Maguire
 Author URI: http://customrayguns.com/
 */
-
+die('South Carolina 50/50 active.');
 namespace CustomRayGuns;
 
 require 'CustomRayGuns/php-autoloader/autoloader.php';
 $CustomProduct = new CustomProduct;
+
