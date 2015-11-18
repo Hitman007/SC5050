82857d5d80fa29007e5ae5624c1d9f252d0416db
Hitman007 <support@customrayguns.com>
2015-11-18 11:05:15 -0500

    Create 5050ProductCept.php

---
+++ tests/acceptance/5050ProductCept.php
@@ -0,0 +1,10 @@
+<?php
+//This tests that the custom products have been added to Woo
+$I = new CustomRayGuns\WordpressAcceptanceTester($scenario);
+$I->wantTo('Make sure that custom products have been added');
+$I->seeThisPluginIsActivated('woocommerce');
+$I->loginWordpressAs('admin');
+global $CRG_homePageURL; //This variable is set in the tests/_bootstrap.php file
+$productsPageURL = $CRG_homePageURL . "/wp-admin/edit.php?post_type=product";
+$I->amOnPage($productsPageURL);
+$I->see('SKU');
