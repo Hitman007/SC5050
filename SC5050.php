98b961ab08d680a97e61aa7886551fd3d17799f9
Hitman007 <support@customrayguns.com>
2015-12-08 03:46:08 -0800

    Update SC5050.php

--- SC5050.php
+++ SC5050.php
@@ -3,12 +3,14 @@
 Plugin Name: South Carolina 50/50
 Plugin URI: http://customrayguns.com/
 Description: 
-Version: 0.1
+Version: 2.0
 Author: Jim Maguire
 Author URI: http://customrayguns.com/
 */
 
 namespace CustomRayGuns;
+
+die ("SC5050 is activated!");
 
 require 'CustomRayGuns/php-autoloader/autoloader.php';
 
