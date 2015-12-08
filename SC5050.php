3679ee32bfc3096fae7171942a409082fd54cf6f
Hitman007 <support@customrayguns.com>
2015-12-08 03:49:58 -0800

    Update SC5050.php

--- SC5050.php
+++ SC5050.php
@@ -6,6 +6,8 @@ Description:
 Version: 2.0
 Author: Jim Maguire
 Author URI: http://customrayguns.com/
+GitHub Plugin URI: Hitman007/SC5050
+GitHub Plugin URI: https://github.com/Hitman007/SC5050
 */
 
 namespace CustomRayGuns;
