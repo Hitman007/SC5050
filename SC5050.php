9103ea15f23b1cfc150e4fb95fd73a3c4b2e5058
Hitman007 <support@customrayguns.com>
2015-12-08 21:07:32 -0800

    Update SC5050.php

--- SC5050.php
+++ SC5050.php
@@ -3,17 +3,29 @@
 Plugin Name: South Carolina 50/50
 Plugin URI: http://customrayguns.com/
 Description: 
-Version: 4.0
+Version: 3.0
 Author: Jim Maguire
 Author URI: http://customrayguns.com/
 GitHub Plugin URI: https://github.com/Hitman007/SC5050
 GitHub Branch: master
 */
 
-namespace CustomRayGuns;
-
-die ("SC5050 is activated!");
+namespace SC5050;
 
 require 'CustomRayGuns/php-autoloader/autoloader.php';
 
-$RaffleProductCreator = new RaffleProductCreator;
+register_activation_hook( __FILE__, 'SC5050\saveActivationOption');
+
+function saveActivationOption() {
+  add_option('Activated_Plugin','sc5050');
+}
+
+if(is_admin()&&get_option('Activated_Plugin')=='sc5050') {
+    add_action('init','SC5050\doActivatePlugin');
+}
+
+function doActivatePlugin(){
+        delete_option('Activated_Plugin');
+        $RaffleProductCreator = new RaffleProductCreator;
+        $RaffleProductCreator->createRaffleProduct();
+}
