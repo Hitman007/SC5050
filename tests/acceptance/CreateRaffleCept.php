1f2f328c5e77243fb3e9df6fa6fce453aa0501cd
Hitman007 <support@customrayguns.com>
2015-12-19 18:05:12 -0800

    Update CreateRaffleCept.php

--- tests/acceptance/CreateRaffleCept.php
+++ tests/acceptance/CreateRaffleCept.php
@@ -18,25 +18,22 @@ Scenario:
 $I->loginWordpressAs('admin');
 
 //Compile the dashboard's URL. Set in _bootstrap.php:
-global $CRG_homePageURL; $dashBoardURL = $CRG_homePageURL . "wp-admin/index.php";
-
-$I->amOnUrl($dashBoardURL);
-$I->see('50/50 Raffles');
+	global $CRG_homePageURL; $dashBoardURL = $CRG_homePageURL . "wp-admin/index.php";
+	$I->amOnUrl($dashBoardURL);
+$I->see('Raffles');
 
 /*
  Sceanrio:
     When an admin clicks the menu item
     Then he sees the admin page
     
-Scenario: The admin can create new raffles
+Scenario: The admin creates a new raffle
 
-Scenario: The admin can edit existing raffles
+Scenario: The admin edits an existing raffle
 
-Scenario: The admin can delete a raffle
+Scenario: The admin deletes a raffle
 
-Scenario: The admin edits raffle details
-    When the admin edits a raffle's details
-    Then he can modify the following fields
+Scenario: The admin sees a detailed list of available raffles
 */
 $I->click(".toplevel_page_raffle_page");
 $I->see('Create New Charitable Raffle');
