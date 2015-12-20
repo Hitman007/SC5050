4fc3a22f439052dcd36e793753375f59a1aa2d11
Hitman007 <support@customrayguns.com>
2015-12-19 19:21:33 -0800

    Update _bootstrap.php

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -3,7 +3,8 @@
 require(dirname(__FILE__)."/../CustomRayGuns/php-autoloader/autoloader.php");
 
 global $CRG_homePageURL;
-$CRG_homePageURL = 'https://crg-remote-hitman007.c9users.io/';
+//$CRG_homePageURL = 'https://crg-remote-hitman007.c9users.io/';
+$CRG_homePageURL = 'http://ec2-52-90-107-126.compute-1.amazonaws.com/';
 
 global $CRG_loginPageURL;
 $CRG_loginPageURL = $CRG_homePageURL . 'wp-login.php';
