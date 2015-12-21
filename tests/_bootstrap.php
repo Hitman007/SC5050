b55fc09a26206a2c5348001492d3ddfd64312e4e
Hitman007 <support@customrayguns.com>
2015-12-20 16:24:06 -0800

    Update _bootstrap.php

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -26,10 +26,8 @@ global $CRG_DBsetupPath; $CRGDBsetupPath = "~/workspace/wp-content/plugins/SC505
 global $CRG_pLuginUnderTestVerboseName; $CRG_pLuginUnderTestVerboseName = "South Carolina 50/50"; $CRG_pluginUnderTestSlugName; $CRG_pluginUnderTestSlugName = "SC5050";
 
 //SC5050
-global $productTextName;
-$productTextName = "50/50 Raffle Ticket";
-global $productPermalink;
-$productPermalink = "product/5050-raffle-ticket/";
+global $productTextName; $productTextName = "50/50 Raffle Ticket";
+global $productPermalink; $productPermalink = "index.php/product/5050-raffle-ticket/";
 
 //Woocommerce
 global $productsPageURL; $productsPageURL = "http://ec2-52-90-107-126.compute-1.amazonaws.com/wp-admin/edit.php?post_type=product";
