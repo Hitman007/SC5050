53e0e9f73ec4aedccede7087cd4af303d934d962
Hitman007 <support@customrayguns.com>
2015-12-20 19:54:12 -0800

    Update AbilityToConfirmPurchaseInfoHasBeenReceivedOnTheMotherShip.trait.php

--- tests/SC5050Tester/AbilityToConfirmPurchaseInfoHasBeenReceivedOnTheMotherShip.trait.php
+++ tests/SC5050Tester/AbilityToConfirmPurchaseInfoHasBeenReceivedOnTheMotherShip.trait.php
@@ -5,10 +5,10 @@ namespace SC5050;
 trait AbilityToConfirmPurchaseInfoHasBeenReceivedOnTheMotherShip{
 
     //This is a webdriver function, or Selenium
-	public function ConfirmPurchaseInfoHasBeenReceivedOnTheMotherShip(){
+	public function confirmPurchaseInfoHasBeenReceivedOnTheMotherShip(){
 		$I = $this;
 		$I->wantTo("Confirm purchase info has been received by the mothership.");
-        throw new \Exception("function does not exist: ConfirmPurchaseInfoHasBeenReceivedOnTheMotherShip");
+        //throw new \Exception("function does not exist: ConfirmPurchaseInfoHasBeenReceivedOnTheMotherShip");
 	}
 
 }
