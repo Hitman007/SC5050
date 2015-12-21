f922a77a0d867a184d0c4095e86b6a884047113f
Hitman007 <support@customrayguns.com>
2015-12-20 19:47:16 -0800

    Update and rename TicketPurchaseIsRegisteredAcrossSitesCept.php to SiteSendsDataToMothershipCept.php

--- tests/acceptance/SiteSendsDataToMothershipCept.php
+++ tests/acceptance/SiteSendsDataToMothershipCept.php
@@ -1,20 +1,24 @@
 <?php
 
 /*
-Feature: The site should send purchase info to the motherShip
+Feature: The site sends data with every purchace to the motherShip
     In order to keep track of purchase information
-    Info is sent with every purchace to the motherShip
+    As the system
+    I should send data with every purchace to the motherShip
 */
-
 $I = /*am a */ new SC5050\SC5050Tester($scenario);
-$I->wantTo("Confirm Feature: The site should send purchase info to the motherShip");
+$I->wantTo("Confirm Feature: The site sends data with every purchace to the motherShip");
 
 /*
 Scenario:
     When a user has purchased a ticket 
     Then the information is sent to $motherShip sendInfoToMotherShip();
 */
-
 global $productPermalink; global $CRG_homePageURL; $productPage = $CRG_homePageURL . $productPermalink;
 $I->purchaseWooProduct($productPage);
 //$I->confirmPurchaseInfoHasBeenReceivedOnTheMotherShip();
+
+/*Scenario:
+    When a user has purchased a ticket
+    Then they receive an email with the ticket info
+*/
