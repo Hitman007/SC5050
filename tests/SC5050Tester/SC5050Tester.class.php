9d59554f572b7d898ef46b2ee1d8aa16f8dcbbe8
Hitman007 <support@customrayguns.com>
2015-12-20 19:54:51 -0800

    Update SC5050Tester.class.php

--- tests/SC5050Tester/SC5050Tester.class.php
+++ tests/SC5050Tester/SC5050Tester.class.php
@@ -3,6 +3,6 @@
 namespace SC5050;
 
 class SC5050Tester extends \CustomRayGuns\WordpressTester{
-    use AbilityToPurchaceATicket;
+    use AbilityToPurchaceTicket;
     use AbilityToConfirmPurchaseInfoHasBeenReceivedOnTheMotherShip;
 }
