ef8782fdd6d158d83e093c10e01278867691a285
Hitman007 <support@customrayguns.com>
2015-12-20 18:09:15 -0800

    Update AbilityToPurchaseWooProduct.trait.php

--- tests/WordpressTester/AbilityToPurchaseWooProduct.trait.php
+++ tests/WordpressTester/AbilityToPurchaseWooProduct.trait.php
@@ -20,24 +20,15 @@ trait AbilitToPurchaseWooProduct{
 		//This class was identified by viewing with Firefox:
 		$I->click("//a[@class='checkout-button button alt wc-forward']");
 		$I->see('Billing Details');
-		$I->fillField('billing_first_name','DeleteFirstName');
+
 		$I->fillField('billing_last_name', 'DeleteLastName');
 		$I->fillField('billing_phone', '(555)555-1212');
 		$I->fillField('billing_email', 'delete@delme.com');
 		$I->fillField('billing_address_1', '123 Mulberry Lane');
 		$I->fillField('billing_address_2', 'Apt 2-b');
-		$I->fillField('billing_city', 'Anytown');
+
 		$I->fillField('billing_postcode', '12345');
-		$I->click('#s2id_billing_state');
-		$I->click('#s2id_billing_state');
-		$I->wait(5);
-		$I->pressKey('.page', 'N');
-		$I->pressKey('.page','e');
-		$I->pressKey('.page','v');
-		$I->pressKey('.page','a');
-		$I->pressKey('.page','d');
-		$I->pressKey('.page','a');
-		$I->pressKey('.page', \WebDriverKeys::ENTER);
+
 		//$I->pressKey('.page', '13');
 		//$I->click('#select2-results-2');
 		//$I->click('.entry-title');
@@ -58,6 +49,24 @@ trait AbilitToPurchaseWooProduct{
 		$I->pressKey('.page','s');
 		//$I->pressKey('.page', '13');
 		$I->pressKey('.page', \WebDriverKeys::ENTER);
+		
+		
+		$I->fillField('billing_first_name','DeleteFirstName');		
+	
+		$I->click('#s2id_billing_state');
+		$I->click('#s2id_billing_state');
+		$I->wait(5);
+		$I->pressKey('.page', 'N');
+		$I->pressKey('.page','e');
+		$I->pressKey('.page','v');
+		$I->pressKey('.page','a');
+		$I->pressKey('.page','d');
+		$I->pressKey('.page','a');
+		$I->pressKey('.page', \WebDriverKeys::ENTER);
+		$I->wait(5);
+		
+		$I->fillField('billing_city', 'Anytown');
+		
 		$I->click('#place_order');
 		$I->wait(25);
 		$I->see('Thank You');
