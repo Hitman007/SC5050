0794cf07efcb7f03ea3fb3978d97f49bf492eb53
Hitman007 <support@customrayguns.com>
2015-12-31 12:49:53 -0800

    Update AbilityToConfirmWoocommerceDefaultPagesExist.trait.php

--- tests/WordpressTester/AbilityToConfirmWoocommerceDefaultPagesExist.trait.php
+++ tests/WordpressTester/AbilityToConfirmWoocommerceDefaultPagesExist.trait.php
@@ -5,6 +5,9 @@ namespace CustomRayGuns;
 
 trait AbilityToConfirmWoocommerceDefaultPagesExist{
     
-    public function confirmWoocommerceDefaultPagesExist(){}
+    public function confirmWoocommerceDefaultPagesExist(){
+        $message = "function confirmWoocommerceDefaultPagesExist() has not been defined.";
+        throw new \Exception($message);
+    }
     
 }
