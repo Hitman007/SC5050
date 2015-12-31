42b8df35d5ac8f9c33e1a8ec9e6f7c6c29fbe6a9
Hitman007 <support@customrayguns.com>
2015-12-31 15:46:04 -0800

    Update AbilityToConfirmWoocommerceDefaultPagesExist.trait.php

--- tests/WoocommerceTester/AbilityToConfirmWoocommerceDefaultPagesExist.trait.php
+++ tests/WoocommerceTester/AbilityToConfirmWoocommerceDefaultPagesExist.trait.php
@@ -10,6 +10,7 @@ trait AbilityToConfirmWoocommerceDefaultPagesExist{
 		global $CRG_homePageURL; //This variable is set in the tests/_bootstrap.php file
 		$checkoutPageURL = $CRG_homePageURL . "checkout/";
 		$I->amOnUrl($checkoutPageURL);
+		$I->doNotSee('Not Found');
     }
     
 }
