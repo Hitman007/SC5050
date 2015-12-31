97970593fdad152c184b49e06fb0a22a2088869a
Hitman007 <support@customrayguns.com>
2015-12-31 15:09:49 -0800

    Create WoocommerceDefaultPagesCept.php

---
+++ tests/acceptance/WoocommerceDefaultPagesCept.php
@@ -0,0 +1,11 @@
+
+<?php
+/*
+Feature: 
+    In order 
+    As as 
+    I want to 
+*/
+$I = /*am a */ new SC5050\SC5050Tester($scenario);
+$I->wantTo("make sure WOO default pages are there");
+$I->confirmWoocommerceDefaultPagesExist();
