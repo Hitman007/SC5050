b1ec2d4ce0de632d473f447ca24a0ed72716d62c
Hitman007 <support@customrayguns.com>
2015-12-31 15:44:10 -0800

    Update AbilityToConfirmWoocommerceDefaultPagesExist.trait.php

--- tests/WoocommerceTester/AbilityToConfirmWoocommerceDefaultPagesExist.trait.php
+++ tests/WoocommerceTester/AbilityToConfirmWoocommerceDefaultPagesExist.trait.php
@@ -5,8 +5,6 @@ namespace CustomRayGuns;
 trait AbilityToConfirmWoocommerceDefaultPagesExist{
     
     public function confirmWoocommerceDefaultPagesExist(){
-        $message = "function confirmWoocommerceDefaultPagesExist() has not been defined.";
-        throw new \Exception($message);
         $I = $this;
         $I->wantTo('Confirm the default Woocommerce pages exist.');
 		global $CRG_homePageURL; //This variable is set in the tests/_bootstrap.php file
