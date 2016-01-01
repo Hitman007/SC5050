5c24d8f7d5ae00a1a3fe5b869f08a895cd403f7e
Hitman007 <support@customrayguns.com>
2016-01-01 13:23:08 -0800

    Update CRG_WP_LoginController.php

--- tests/_support/Step/Acceptance/CRG_WP_LoginController.php
+++ tests/_support/Step/Acceptance/CRG_WP_LoginController.php
@@ -4,12 +4,6 @@ namespace Step\Acceptance;
 class CRG_WP_LoginController extends \AcceptanceTester
 {
 
-    public function loginAsAdmin()
-    {
-        $I = $this;
-        $I->wantToTest('xxxxxxxxxxxxxxx');
-        $I->addStep('vvvvvvvvvvvvvvv');
-    	$I->amOnUrl('http://google.com');
-    }
+
 
 }
