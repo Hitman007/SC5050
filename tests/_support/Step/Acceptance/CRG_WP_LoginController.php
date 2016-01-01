0c882f2f24d48ae6ed11aaafa2a5e6cc9d5e36e0
Hitman007 <support@customrayguns.com>
2016-01-01 13:05:36 -0800

    Update CRG_WP_LoginController.php

--- tests/_support/Step/Acceptance/CRG_WP_LoginController.php
+++ tests/_support/Step/Acceptance/CRG_WP_LoginController.php
@@ -7,7 +7,7 @@ class CRG_WP_LoginController extends \AcceptanceTester
     public function loginAsAdmin()
     {
         $I = $this;
-        $I->see('Admin Profile'); 
+    	$I->amOnUrl('http://google.com');
     }
 
 }
