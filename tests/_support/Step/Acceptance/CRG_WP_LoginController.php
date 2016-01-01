62917e81e503cac9c1e539fdcbf50d918d3e7a2e
Hitman007 <support@customrayguns.com>
2016-01-01 13:11:06 -0800

    Update CRG_WP_LoginController.php

--- tests/_support/Step/Acceptance/CRG_WP_LoginController.php
+++ tests/_support/Step/Acceptance/CRG_WP_LoginController.php
@@ -6,6 +6,7 @@ class CRG_WP_LoginController extends \AcceptanceTester
 
     public function loginAsAdmin()
     {
+        $I->wantTo('generate invitation code and sign up using it');
         $I = $this;
     	$I->amOnUrl('http://google.com');
     }
