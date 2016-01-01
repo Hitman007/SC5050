cd4b29bed34fb9361a4a78ba8983ac8be312cbfc
Hitman007 <support@customrayguns.com>
2016-01-01 10:48:09 -0800

    Update AbilityToConfirmWoocommerceDefaultPagesExist.trait.php

--- tests/WoocommerceTester/AbilityToConfirmWoocommerceDefaultPagesExist.trait.php
+++ tests/WoocommerceTester/AbilityToConfirmWoocommerceDefaultPagesExist.trait.php
@@ -7,9 +7,16 @@ trait AbilityToConfirmWoocommerceDefaultPagesExist{
     public function confirmWoocommerceDefaultPagesExist(){
         $I = $this;
         $I->wantTo('Confirm the default Woocommerce pages exist.');
+        
 		global $CRG_homePageURL; //This variable is set in the tests/_bootstrap.php file
+		
 		$checkoutPageURL = $CRG_homePageURL . "/index.php/checkout/";
 		$I->amOnUrl($checkoutPageURL);
 		$I->dontSee('Not Found');
+		
+		$cartPageURL = $CRG_homePageURL . "/index.php/cart/";
+		$I->amOnUrl($cartPageURL);
+		$I->dontSee('Not Found');
+		$I->see('UPDATE CART');
     }
 }
