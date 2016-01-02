bd248ddfca4cc12ebad237490bd18cb6aea8feca
Hitman007 <support@customrayguns.com>
2016-01-02 10:13:43 -0800

    Update AdminPageView.class.php

--- CustomRayGuns/AdminPageView.class.php
+++ CustomRayGuns/AdminPageView.class.php
@@ -5,10 +5,10 @@ namespace SC5050;
 class AdminPageView{
 	
 	public function __construct() {
-		add_menu_page( 'custom menu title', '50/50 Raffles', 'manage_options', 'raffle_page', array ($this, 'my_custom_menu_page'), 'dashicons-tickets-alt', 6 );
+		add_menu_page( 'custom menu title', '50/50 Raffles', 'manage_options', 'raffle_page', array ($this, 'echoSC5050adminPage'), 'dashicons-tickets-alt', 6 );
 	}
 	
-	public function my_custom_menu_page(){
+	public function echoSC5050adminPage(){
 $output = "admin area";
 $output = $output . $this->mothershipAdminArea();
 		echo $output;
