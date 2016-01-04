aa304c8c7e64800ba0e885cfe824497339b452cd
Hitman007 <support@customrayguns.com>
2016-01-04 06:51:37 -0800

    Create ticketEmailHandler.class.php

---
+++ CustomRayGuns/ticketEmailHandler.class.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SC5050;
+
+//This class is instantiated when the 'thank-you' event is triggered from Woocommerce
+
+class ticketEmailHandler{
+
+	public function doSendCustomerTicketEmail(){}
+
+}
