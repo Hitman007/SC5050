3096e379667baa77ff30b5d8f7ae352156fde027
Hitman007 <support@customrayguns.com>
2016-01-05 15:17:53 -0800

    Update getAddRaffleFormHTML.php

--- CustomRayGuns/getAddRaffleFormHTML.php
+++ CustomRayGuns/getAddRaffleFormHTML.php
@@ -7,6 +7,9 @@ function getAddRaffleFormHTML(){
 $output = <<<formOutput
 <form name = 'crg-add-raffle-form' id = 'crg-add-raffle-form' method = 'post' />
 
+<label for = '' ></label>
+<input type = 'text' />
+
 </form>
 formOutput;
 
