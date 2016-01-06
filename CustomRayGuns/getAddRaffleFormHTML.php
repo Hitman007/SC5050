a52965233c51c6a27a000e24e7abac0765be79f2
Hitman007 <support@customrayguns.com>
2016-01-05 19:52:36 -0800

    Update getAddRaffleFormHTML.php

--- CustomRayGuns/getAddRaffleFormHTML.php
+++ CustomRayGuns/getAddRaffleFormHTML.php
@@ -3,18 +3,24 @@
 namespace SC5050;
 
 function getAddRaffleFormHTML(){
-die('line 6 getAddRaffleFormHTML');
-$output = <<<formOutput
+	die('line 6 getAddRaffleFormHTML');
+	$output = <<<formOutput
 <form name = 'crg-add-raffle-form' id = 'crg-add-raffle-form' method = 'post' />
 
 <label for = 'raffle-name' >Raffle Name</label>
 <input type = 'text' id = 'raffle-name' />
 
-<input type = 'submit' />
+<label for = 'raffle-date' >Raffle Date</label>
+<input type = 'text' id = 'raffle-date' />
+
+<label for = 'raffle-time'>Raffle Time</label>
+<input type = 'text' id = 'raffle-time' />
+
+<input type = 'submit' value 'Submit' name = 'raffle-add-submit' />
 
 </form>
+
 formOutput;
 
-return $output;
-
+	return $output;
 }
