017650c9dd0c92e33fbbd7b9b7c834e4b28bbacc
Hitman007 <support@customrayguns.com>
2016-11-13 20:09:19 -0800

    hi

--- CustomRayGuns/AddTicketMetaBoxListener.php
+++ CustomRayGuns/AddTicketMetaBoxListener.php
@@ -7,27 +7,20 @@ class AddTicketsMetaBoxListener{
 	//This class should only be called once when the listen condition is met.
 	
 	public function __construct(){
-		$this->validateIncoming();
-		die ("AddTicketsMetaBoxListener line 11");
-	}
-	
-	public function validateIncoming(){
-		$ticketOne = $_POST['SC5050-ticket-one'];
-		//$tmr = $_POST[''];
-		//$to = $_POST[''];
-		//$tmr = (int)$ticketOne;
-		echo ($ticketOne) . "<br />";
-		if (is_int($ticketOne)){
-			die("validoo");
-		}else{
-			die("INVALID!");
+		
+		if (isset($_POST['SC5050-ticket-one'])){
+			if(isset($_POST['SC5050-hidden-post-id'])){
+				$PRTN = $_POST['SC5050-ticket-one'];
+				$postID  = $_POST['SC5050-hidden-post-id'];
+				$this->createPRTNTicket($postID, $PRTN);
+			}
 		}
 	}
-	
+		
 	public function registerError(){}
 	
 	public function createPRTNTicket($postID, $PRTN){
-		
+		die('createPRTNTicket');
 		//check if the PRTN already exist
 		//throw error
 	}
